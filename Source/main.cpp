96c70b3fe11bc7f540eeb3eb67f9ae1e43d41844
Phantom <phantom@phantom-games.com>
2025-01-04 09:45:16 -0800

    Fix for bad scaling on high resolution displays in Windows when using anything other than 100% scale

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,8 +2,15 @@
 
 #include "GUI/MainWindow.h"
 
+#ifdef _MSC_VER
+#include <Windows.h>
+#endif
+
 int main(int argc, char** argv)
 {
+  #ifdef _MSC_VER
+  SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_UNAWARE_GDISCALED);
+  #endif
   QApplication app(argc, argv);
   MainWindow window;
   window.show();
